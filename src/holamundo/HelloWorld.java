16b2ac5fdddaa25b6fd2a808934c913dea5e32c7
apadlop724 <apadlop724@g.educaand.es>
2025-04-02 03:07:56 +0200

    Update HelloWorld.java

--- src/holamundo/HelloWorld.java
+++ src/holamundo/HelloWorld.java
@@ -5,9 +5,7 @@ public class HelloWorld {
 	// Tu programa comienza con una llamada a main()
 	// Imprime "Hola Mundo" a la ventana de la terminal. 
 	public static void main(String[] args) {		
-		
-		System.out.println("Hola Mundo");
-
+		System.out.println("Hola Mundo desde GitHub");
 	}
 
 }
